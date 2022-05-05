6f511f00583f14cd07c004e6c26d2e62847d3c70
Michael Vann <michaelwvann@gmail.com>
2022-05-05 13:45:44 +1000

    Added a screen resolution and safe area read out on the options screen

--- Assets/Scripts/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -7,8 +7,10 @@ public class OptionsMenu : MonoBehaviour
 {
     GameHandler m_gameHandlerRef;
     public Slider m_xpRewardScaleSlider;
-    public Text m_xpRewardText;
-    // Start is called before the first frame update
+    public Text m_xpRewardTextRef;
+    public Text m_resolutionTextRef;
+    public Text m_safeAreaTextRef;
+
     void Start()
     {
         m_gameHandlerRef = FindObjectOfType<GameHandler>();
@@ -17,13 +19,14 @@ public class OptionsMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        m_resolutionTextRef.text = "Resolution: " + Screen.width + " x " + Screen.height;
+        m_safeAreaTextRef.text = "Safe Area: " + Screen.safeArea.width + " x " + Screen.safeArea.height;
     }
 
     public void OnXPRewardSliderChange()
     {
         m_gameHandlerRef.m_enemyXPScale = m_xpRewardScaleSlider.value;
-        m_xpRewardText.text = "" + m_xpRewardScaleSlider.value;
+        m_xpRewardTextRef.text = "" + m_xpRewardScaleSlider.value;
     }
 
     void OnXPRequirementSliderChange()
