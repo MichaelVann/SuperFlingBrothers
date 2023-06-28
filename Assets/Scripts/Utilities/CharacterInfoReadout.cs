ef974902131af4b2db3b867803b2a5f819daabed
Michael Vann <michaelwvann@gmail.com>
2023-06-28 13:24:00 +1000

    Changed the central equipment readout to not be so mono-colored

--- Assets/Scripts/Utilities/CharacterInfoReadout.cs
+++ Assets/Scripts/Utilities/CharacterInfoReadout.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class CharacterInfoReadout : MonoBehaviour
 {
+    public Text[] m_statNameTexts;
     public Text[] m_statTexts;
     GameHandler m_gameHandlerRef;
     // Start is called before the first frame update
     void Start()
     {
         m_gameHandlerRef = FindObjectOfType<GameHandler>();
+        for (int i = 0; i < m_statNameTexts.Length; i++)
+        {
+            m_statNameTexts[i].color = CharacterStatHandler.GetStatColor(i);
+        }
     }
 
     public void Refresh()
@@ -20,12 +25,13 @@ public class CharacterInfoReadout : MonoBehaviour
             m_statTexts[i].text = "";
         }
 
+
         for (int i = 0; i < m_statTexts.Length && i < (int)eCharacterStatIndices.count; i++)
         {
             CharacterStat stat = m_gameHandlerRef.m_playerStatHandler.m_stats[i];
-            m_statTexts[i].text = stat.name + ": " + stat.finalValue;
+            m_statTexts[i].text = "" + stat.finalValue;
             m_statTexts[i].text += "(+" + stat.equipmentAddedValue + ") (+" + stat.effectiveValue + ")";
-            m_statTexts[i].color = CharacterStatHandler.GetStatColor((eCharacterStatIndices)i);
+            m_statTexts[i].color = Color.white;// CharacterStatHandler.GetStatColor((eCharacterStatIndices)i);
         }
     }
 
