8c561186cd46cdfbf942a94fbc6b486bc77d514c
Michael Vann <michaelwvann@gmail.com>
2023-08-13 11:55:48 +1000

    Version 17.7 - Added Wall Cracks

--- Assets/Scripts/MainGame/GameHandler.cs
+++ Assets/Scripts/MainGame/GameHandler.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
-    public const float _VERSION_NUMBER = 17.5f;
+    public const float _VERSION_NUMBER = 17.7f;
 
     static internal bool DEBUG_MODE = true;
 
