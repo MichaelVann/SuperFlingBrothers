55ed83d2774ee382a065d094763433daf722fa7c
Michael Vann <michaelwvann@gmail.com>
2023-09-04 18:26:18 +1000

    Version 19.2 - Fixed a weird corruption glitch for the nucleus Updated the Healer's shadow to the correct sprite

--- Assets/Scripts/Battle/Nucleus.cs
+++ Assets/Scripts/Battle/Nucleus.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Nucleus : Damageable
 {
-
     public override void Awake()
     {
         base.Awake();
-        m_statHandler.m_stats[(int)eCharacterStatIndices.constitution].finalValue = 80f;
-        UpdateLocalStatsFromStatHandler();
+        //m_statHandler.m_stats[(int)eCharacterStatIndices.constitution].finalValue = 80f;
+        //UpdateLocalStatsFromStatHandler();
         m_damageTextColor = Color.red;
     }
 
@@ -17,13 +17,9 @@ public class Nucleus : Damageable
     {
         base.Start();
         m_gameHandlerRef = FindObjectOfType<GameHandler>();
-        //int difficulty = m_gameHandlerRef.m_battleDifficulty < 10 ? 10 : m_gameHandlerRef.m_battleDifficulty;
-        //float exponent = 0.3f;
-        //m_statHandler.m_stats[(int)eCharacterStatIndices.constitution].finalValue *= Mathf.Pow(difficulty, exponent) /Mathf.Pow(10f, exponent);
         m_statHandler.m_stats[(int)eCharacterStatIndices.constitution].finalValue = 1000f;
         UpdateLocalStatsFromStatHandler();
         if (m_healthBarRef) { m_healthBarRef.SetProgressValue(m_health); }
-
     }
 
     public override void Update()
--- Assets/Scripts/MainGame/GameHandler.cs
+++ Assets/Scripts/MainGame/GameHandler.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
-    public const float _VERSION_NUMBER = 19.1f;
+    public const float _VERSION_NUMBER = 19.2f;
 
     static internal bool DEBUG_MODE = true;
 
