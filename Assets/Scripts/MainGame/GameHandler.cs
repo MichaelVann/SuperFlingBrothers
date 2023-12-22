c144686a5c776cdd463986209ca15c737ebe03d3
Michael Vann <michaelwvann@gmail.com>
2023-12-22 16:31:03 +1000

    Version 27.5 - Replaced Title triangle with a hand drawn version

--- Assets/Scripts/MainGame/GameHandler.cs
+++ Assets/Scripts/MainGame/GameHandler.cs
@@ -17,7 +17,7 @@ public class GameHandler : MonoBehaviour
     internal static GameHandler m_staticAutoRef;
 
     public const int MAIN_VERSION_NUMBER = 27;
-    public const int SUB_VERSION_NUMBER = 4;
+    public const int SUB_VERSION_NUMBER = 5;
 
     static internal bool DEBUG_MODE = true;
 
